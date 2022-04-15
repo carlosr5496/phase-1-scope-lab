ffed7b746470331bd7570df316ab828523d79fa6
Ryan Carlos <ryancarlos925@gmail.com>
2022-04-15 14:30:25 -0700

    commit tree de300472fe2b3eb4e0957c770efa8a9c41df6638

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+var bestCustomer;
+
+var customerName = "bob";
+
+const leastFavoriteCustomer = "billy";
+
+function toUpperCase() {
+    customerName = "BOB"
+    return customerName
+}
+
+
+function upperCaseCustomerName() {
+    let  upperName = toUpperCase();
+    return upperName
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+    return bestCustomer
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+    return bestCustomer
+}
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = "joe";
+    return leastFavoriteCustomer
+}
